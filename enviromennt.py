55b2bd303f3379bbf3da4f0de20a35fb713f51d5
jersonval96 <jersonDevelopment@gmail.com>
2021-11-01 21:08:39 -0500

    quitando credenciales

--- enviromennt.py
+++ enviromennt.py
@@ -7,8 +7,8 @@ Created on Mon Nov  1 16:53:44 2021
 
 import os
 
-os.environ['SENDGRID_API_KEY'] = "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"
+os.environ['SENDGRID_API_KEY'] = ""
 os.environ['SECURITY_HASH'] = "Jer"
 os.environ['Email_from'] = 'jerson.1701821048@ucaldas.edu.co'
-os.environ['TWILIO_ACCOUNT_SID'] = 'AC720e63fab6b7822dc0d6c9bb0873c0cd'
-os.environ['TWILIO_AUTH_TOKEN'] = '364b41f3ddb80d7011f63708646e5473'
+os.environ['TWILIO_ACCOUNT_SID'] = ''
+os.environ['TWILIO_AUTH_TOKEN'] = ''
